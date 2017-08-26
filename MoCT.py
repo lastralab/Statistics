763d4a3c87ad3c61a920cc23dfc6ac40c9f8be84
L'AstraLab <mariametropolis8.5@gmail.com>
2017-08-26 13:40:07 -0300

    Update MoCT.py

--- MoCT.py
+++ MoCT.py
@@ -72,7 +72,7 @@ plt.title("Population distribution")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
 plt.legend()
-print ('To continue, you must save the figure and close it.\n')
+print ('To continue, you must save the figure and close it. You can also zoom in it or move the graph to see it better, use the buttons.\n')
 plt.show(fig)
 
 while True:
@@ -82,9 +82,9 @@ while True:
     if fh == 'ya':
         break
     newn = float(fh)
-    newdeviation = StdDev / math.sqrt(newn)
+    standarderror = StdDev / math.sqrt(newn)
     print('----------------------------------------------------------------')
-    print 'The Standard Error for', newn,'is: ', newdeviation
+    print 'The Standard Error for', newn,'is: ', standarderror
     print('----------------------------------------------------------------')
     print ' '
     anyvalue = raw_input('Enter point estimate: ')
@@ -94,19 +94,18 @@ while True:
         break
     newmean = float(anyvalue)
     rest = newmean - mean
-    zscore = rest / newdeviation
+    zscore = rest / standarderror
     print ' '
     print '| The z-score for', anyvalue, 'is:', zscore, '|'
     pvalue = st.norm.cdf(zscore)
     print('---------------------------------------------')
     print '| Z table value =', pvalue, '|'
     print('---------------------------------------------')
-    greater = 1 - pvalue
+    prob = 1 - pvalue
     print 'The probability of getting at least', anyvalue,'is:'
-    print 'p =', greater
-    percent = greater * 100
-    print 'p =', percent,'%'
-    Devs = (1.96 * newdeviation)
+    print ' '
+    print 'p =', prob
+    Devs = (1.96 * standarderror)
     print '--------------------------------------------------------------'
     print 'Approximately 95% of the sample means fall within', Devs, '\nof', mean, '(Population mean)'
     Dev1 = mean - Devs
@@ -114,18 +113,26 @@ while True:
     print ' '
     print 'The 95% confidence interval is:', Dev1,'<', newmean, '<', Dev2
     print ' '
-    Devss = (2.33 * newdeviation)
+    Devss = (2.33 * standarderror)
+    # Some notes for T-test:
+    # Ho = (null hypothesis):
+    #       Mean = Intervention Mean:
+    #       - The sample mean falls somewhere out the critical region.
+    # Ha = (alternative hypothesis):
+    #       M != Intervention Mean ( < o > ):
+    #       - The sample mean falls somewhere in the critical region.
     Dev3 = mean - Devss
     Dev4 = mean + Devss
     print 'The 98% confidence interval is:', Dev3,'<', newmean, '<', Dev4
     print ' '
     print 'The margin of error is:', Devs, 'for 95% and:', Devss,'for 98%'
     print '--------------------------------------------------------------'
-    pdf2 = stats.norm.pdf(Array, mean, StdDev)
+    print ' '
+    pdf2 = stats.norm.pdf(Array, mean, standarderror)
     altn = int(newn)
     legen = ("n = "+str(altn))
     fig2 = plt.plot(Array, pdf2, label=legen)
-    plt.title("Sampling distribution + 95% Confidence interval")#or 98%
+    plt.title("Sampling distribution & 95% Confidence interval")#or 98%
     plt.xlabel("Value")
     plt.ylabel("Frequency")
     altm = str(mean)
@@ -133,13 +140,13 @@ while True:
     plt.axvline(x= mean, color='r', linestyle='dashed', label=legenda)
     plt.axvline(x= Dev1, color ='g', linestyle='dashed', label=Dev1)
     plt.axvline(x= Dev2, color = 'g', linestyle='dashed', label=Dev2)
-    zscorev = mean+(zscore*newdeviation)
+    zscorev = mean+(zscore*standarderror)
     zscor = ("z-score =\n"+str(zscore))
     plt.axvline(x= zscorev, color = 'purple', label=zscor)
     # Uncomment next two lines for 98% confidence interval
     #plt.axvline(x= Dev3, color = 'orange', linestyle='dashed', label=Dev3)
     #plt.axvline(x= Dev4, color = 'orange', linestyle='dashed', label=Dev4)
-    print ('To continue, you must save the figure and close it, or just close it.\n')
+    print ('To continue, you must save the figure and close it, or just close it. You can also zoom in it or move the graph to see it better, use the buttons.\n')
     plt.legend()
     plt.show(fig2)
     continue
