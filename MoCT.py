d7109fec7f62a6e10d1428b3f537a46d624b0c6e
Niam Moltta <lastralab@gmail.com>
2017-09-21 16:24:29 -0300

    Add files via upload

--- MoCT.py
+++ MoCT.py
@@ -3,83 +3,138 @@
 # Author: Niam Moltta
 # UY - 2017
 # MIT License
+# Measures of Central Tendency with Python
+
 import math
 import numpy as np
 import pandas as pd
 from scipy import stats
 from scipy.stats import norm
-import scipy.stats as stats
 import scipy.stats as st
-import matplotlib
 import matplotlib.pyplot as plt
 import re
+import scipy.stats
+import matplotlib.mlab as mlab
 
 print(' ')
-print(' Welcome to MoCTpy! ')
-print(' --by Tania Mol-- ')
+print(' ')
+print('           Welcome to MoCT.py')
+print('           --by Niam Moltta--')
+print(' ')
 print(' ')
 print("INSTRUCTIONS:\n \n- Make sure that the .txt file is in the same folder of this script, otherwise it won't be found.\n- To start, enter the name of the file without 'quotes' and ending with .txt (example: scores.txt).\n- The name of the file will be the legend on the first plot, I recommend you to choose the name wisely.\n- Later, when you are done selecting values for sample distribution, type 'ya', hit Return and the program will finish nicely.\n")
-fhand = raw_input('Now, enter the file name: ')
 
-numbers = open(fhand)
+fhand = raw_input('Enter file name: ')
 
-A = list()
-for number in numbers :
-    value = float(number)
-    A.append(value)
+filecsv = str(fhand)
 
-sigma = sum(A) #sumation
-n = len(A) #total of elements
-mean = sigma / n
-Dev = list()
-AbsDev = list()
-SqDev = list()
-for number in A :
-    val = number - mean
-    Dev.append(val) #Deviation from the mean
+if filecsv == '':
+    exit()
     
-for element in Dev :
-    val = abs(element)
-    AbsDev.append(val) # Absolute Deviation
-    
-for element in AbsDev :
-    val = (element**2)
-    SqDev.append(val) #Square Deviations
+data = pd.read_csv(filecsv)
 
-SS = sum(SqDev) #Sum of Squares
-Var = SS / n #Variance
-StdDev = math.sqrt(Var) #Standard Deviation
-StdE = StdDev / math.sqrt(n) #Standard Error
+print ' '
 
-print ('---------------------------------------------')
-print ('MEASURES OF CENTRAL TENDENCY for:'), fhand
-print (' ')
-print ('N ='), n
-print ('Mean ='), mean
-print ('Sum of Squares ='), SS
-print ('Variance ='), Var
-print ('Population Standard Deviation ='), StdDev
-print ('Standard Error ='), StdE
-print ('---------------------------------------------')
-print(' ')
-Array = np.asarray(A)
-Array.sort()
-#pdf = stats.norm.pdf(Array, mean, StdDev)
-#fig = plt.plot(Array, pdf)
-legend = re.findall('(.*).txt', fhand)
-fig = plt.hist(Array, color='c', bins=20, histtype='stepfilled', label=legend) # to see the behavior better, set bins for resolution
-plt.title("Population distribution")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.legend()
-print ('To continue, you must save the figure and close it. You can also zoom in it or move the graph to see it better, use the buttons.\n')
-plt.show(fig)
+frame = pd.DataFrame(data)
+
+coolist = frame.columns
+columns = np.asarray(coolist)
 
 while True:
-    fh = raw_input('Enter value for sample distribution: ')
+
+    print ' '
+    print 'Your variables are:\n'
+    print columns
+    print ' '
+    
+    hand = raw_input('Enter column header:\n\n')
+
+    column = str(hand)
+
+    if (column == 'ya') | (column == ''):
+        print ' '
+        print 'Ready for sampling distribution!'
+        print ' '
+        break
+    
+    else:
+        
+        numbers = data[column]
+
+        data[column].fillna(0,inplace=True) # Missing values to zeros.
+            
+        A = list()
+        for number in numbers :
+            value = float(number)
+            A.append(value)
+
+        sigma = sum(A) #sumation
+        n = len(A) #total of elements
+        mean = sigma / n
+        Dev = list()
+        AbsDev = list()
+        SqDev = list()
+        for number in A :
+            val = number - mean
+            Dev.append(val) #Deviation from the mean
+            
+        for element in Dev :
+            val = abs(element)
+            AbsDev.append(val) # Absolute Deviation
+            
+        for element in AbsDev :
+            val = (element**2)
+            SqDev.append(val) #Square Deviations
+
+        SS = sum(SqDev) #Sum of Squares
+        Var = SS / n #Variance
+        StdDev = math.sqrt(Var) #Standard Deviation
+        StdE = StdDev / math.sqrt(n) #Standard Error
+
+        print ('---------------------------------------------')
+        print ('MEASURES OF CENTRAL TENDENCY for:'), fhand
+        print (' ')
+        print ('N ='), n
+        print ('Mean ='), mean
+        print ('Sum of Squares ='), SS
+        print ('Variance ='), Var
+        print ('Standard Deviation ='), StdDev
+        print ('Standard Error ='), StdE # optional
+        print ('---------------------------------------------')
+        print(' ')
+        
+        Array = np.asarray(A)
+        lista = Array.sort()
+        legend = str(column) + ' distribution'
+
+        Mean = np.mean(Array)
+        Variance = np.var(Array)
+        Sigma = np.sqrt(Variance)
+
+        plt.figure(1)
+        plt.hist(Array, bins=20, normed=True)
+        plt.xlim((min(Array), max(Array)))
+
+        x = np.linspace(min(Array), max(Array), n)
+        fig = plt.plot(x, mlab.normpdf(x,Mean,Sigma))
+
+        plt.title(legend)
+        plt.xlabel("Value")
+        plt.ylabel("Frequency")
+        plt.show(fig)
+
+        print ('To continue, you must save the figure and close it. You can also zoom in it or move the graph to see it better, use the buttons.\n')
+        print ' '
+
+######################################################################
+
+while True:
+    
+    fh = raw_input('Enter n value for sample distribution: ')
     if fh[0] == '#':
         continue
     if fh == 'ya':
+        print  ' '
         break
     newn = float(fh)
     standarderror = StdDev / math.sqrt(newn)
@@ -91,6 +146,7 @@ while True:
     if anyvalue[0] == '#':
         continue
     if anyvalue == 'ya':
+        print  ' '
         break
     newmean = float(anyvalue)
     rest = newmean - mean
@@ -105,8 +161,9 @@ while True:
     print 'The probability of getting at least', anyvalue,'is:'
     print ' '
     print 'p =', prob
+    print ' '
+    print '-------------------------------------------------------------'
     Devs = (1.96 * standarderror)
-    print '--------------------------------------------------------------'
     print 'Approximately 95% of the sample means fall within', Devs, '\nof', mean, '(Population mean)'
     Dev1 = mean - Devs
     Dev2 = mean + Devs
@@ -114,25 +171,35 @@ while True:
     print 'The 95% confidence interval is:', Dev1,'<', newmean, '<', Dev2
     print ' '
     Devss = (2.33 * standarderror)
-    # Some notes for T-test:
-    # Ho = (null hypothesis):
-    #       Mean = Intervention Mean:
-    #       - The sample mean falls somewhere out the critical region.
-    # Ha = (alternative hypothesis):
-    #       M != Intervention Mean ( < o > ):
-    #       - The sample mean falls somewhere in the critical region.
+    ''' Some notes for T-test:
+     Ho = (null hypothesis):
+           Mean = Intervention Mean:
+           - The sample mean falls somewhere out the critical region.
+     Ha = (alternative hypothesis):
+           Mean != Intervention Mean (two-tailed),
+           Mean < Intervention Mean (one-tailed - right side),
+           Mean > Intervention Mean (one-tailed - left side):
+           - The sample mean falls somewhere in the critical region.'''
     Dev3 = mean - Devss
     Dev4 = mean + Devss
     print 'The 98% confidence interval is:', Dev3,'<', newmean, '<', Dev4
     print ' '
-    print 'The margin of error is:', Devs, 'for 95% and:', Devss,'for 98%'
+    print 'The margin of error is:', Devs, 'for 95% \nand:', Devss,'for 98%'
     print '--------------------------------------------------------------'
     print ' '
+    if (zscore < 1.96)&(zscore > -1.96):
+        print 'Alpha = 0.05\nHo = Accepted' # Alpha is 0.05 because is used more often
+        print ' '
+        print '--------------------------------------------------------------'
+    else:
+        print 'Alpha = 0.05\nHo = Rejected'
+        print ' '
+        print '--------------------------------------------------------------'
     pdf2 = stats.norm.pdf(Array, mean, standarderror)
     altn = int(newn)
     legen = ("n = "+str(altn))
     fig2 = plt.plot(Array, pdf2, label=legen)
-    plt.title("Sampling distribution & 95% Confidence interval")#or 98%
+    plt.title("Sampling distribution\nalpha = 0.05") # or 98% if you uncomment some lines below
     plt.xlabel("Value")
     plt.ylabel("Frequency")
     altm = str(mean)
@@ -143,13 +210,10 @@ while True:
     zscorev = mean+(zscore*standarderror)
     zscor = ("z-score =\n"+str(zscore))
     plt.axvline(x= zscorev, color = 'purple', label=zscor)
-    # Uncomment next two lines for 98% confidence interval
-    #plt.axvline(x= Dev3, color = 'orange', linestyle='dashed', label=Dev3)
-    #plt.axvline(x= Dev4, color = 'orange', linestyle='dashed', label=Dev4)
     print ('To continue, you must save the figure and close it, or just close it. You can also zoom in it or move the graph to see it better, use the buttons.\n')
     plt.legend()
     plt.show(fig2)
     continue
 print(' ')
-print 'It was a pleasure to make your life easier.\nHasta la vista, baby.'
+print 'Ciao, human!'
 print(' ')
