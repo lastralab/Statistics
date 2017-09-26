e559276a544d7dd73a8a2526e0e96d7cfc1bdc0e
Niam Moltta <lastralab@gmail.com>
2017-09-26 18:27:10 -0300

    Create ANowoa.py

---
+++ ANowoa.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# Author: Niam Moltta
+# UY - 2017
+# License : MIT
+# One way or another...
+# One and Two ways ANOVA conducting with Python
+
+import pandas as pd
+import matplotlib.pylab as plt
+from sklearn import preprocessing
+import re
+import numpy as np
+import statsmodels
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
