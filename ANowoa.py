0003aa224ff7676e33b141431eebb9013097fc30
Niam Moltta <lastralab@gmail.com>
2017-09-29 00:08:16 -0300

    Add files via upload

--- ANowoa.py
+++ ANowoa.py
@@ -7,13 +7,19 @@
 # One and Two ways ANOVA conducting with Python
 
 import pandas as pd
-import plotly.plotly as plt
+from pandas.tools import plotting
+import matplotlib.pyplot as plt
 import plotly.graph_objs as grow 
 import plotly.figure_factory as FF
 import re
 import numpy as np
-import statsmodels
+import seaborn
+import statsmodels.formula.api as ols
+from statsmodels.stats.anova import anova_lm
+from statsmodels.graphics.factorplots import interaction_plot
 from scipy import stats
+import warnings
+
 
 print ' '
 print ' '
@@ -81,7 +87,6 @@ while True:
         print ' '
         grps = pd.unique(data[column2].values)
         d_data = {grp:data[column1][data[column2] == grp] for grp in grps}
-        print ' '
         k = len(pd.unique(data[column2]))
         N = len(data.values)
         n = data.groupby(data[column2]).size()
@@ -134,45 +139,98 @@ while True:
         print 'eta-squared =', effsize
         print ' '
         om_sqrd = ((SSbetween-(DFbetween*MSwithin))/(SStotal+MSwithin))
-        print 'Omega squared =', om_sqrd
+        print 'Omega squared =', om_sqrd      
         print ' '
-        
-        #
-        
+        print 'Drawing scatter plot...'
         print ' '
+        arr = np.asarray(sorted(data[column1]))
+        arrG = np.asarray(sorted(data[column2]))
+        name = str(column1)+' / '+str(column2)
+        warnings.filterwarnings('ignore')
+        fig1 = plt.scatter(arr, arrG, label=name)
+        plt.title(name)
+        plt.ylim((min(arrG)-1, max(arrG)+1))
+        plt.xlabel(column1)
+        plt.ylabel(column2)
+        plt.show(fig1)
 
     elif hm == '2':
 
         print 'Columns in', re.findall('(.+?).csv', filecsv), 'are:\n'
         print columns
         print ' '
-                
-        hand1 = raw_input('Enter first column header: ')
-        print ' '  
-                
-        if (hand1 == 'ya'):
+        
+        #hand0 = raw_input('Enter the ID column name: ')
+        
+        hand1 = raw_input('Enter "by group" data: ')
+        print ' '
+        
+        if hand1 == 'ya':
             print ' '
             continue
         elif hand1 == '':
             break
                 
-        hand2 = raw_input('Enter second column header: ')
+        hand2 = raw_input('Enter X data: ')
         print ' '
 
-        hand3 = raw_input('Enter third column header: ')
+        hand3 = raw_input('Enter Y data: ')
         print ' '
-                
-        column1 = str(hand1)
-        column2 = str(hand2)
-        column3 = str(hand3)
 
-        # TWO WAYS ANOVA:
+        #column1 = str(hand0)
+        column2 = str(hand1)
+        column3 = str(hand2)
+        column4 = str(hand3)
 
+        # TWO WAYS:
+
+        print 'Drawing preview of data'
+        
+        plotting.scatter_matrix(data[[column2, column3, column4]])
+        plt.show()
+        print '1'
+        stats.ttest_ind(data[column3], data[column4])
+        print '2'
+        stats.ttest_rel(data[column3], data[column4])
+        print '3'
+        stats.ttest_1samp(data[column3], data[column4])
+        print '4'
+        plt.show()
+        
+        '''
+        WORKING ON IT:
+        figs = interaction_plot(data.dose, data.supp, data.len,
+             colors=['red','blue'], markers=['D','^'], ms=10)
+
+        N = len(data.len)
+        df_a = len(data.supp.unique()) - 1
+        df_b = len(data.dose.unique()) - 1
+        df_axb = df_a*df_b 
+        df_w = N - (len(data.supp.unique())*len(data.dose.unique()))
+
+        grand_mean = data['len'].mean()
+
+        #datas = [[column2, column3, column4]]
+
+        formula = 'len ~ C(supp) + C(dose) + C(supp):C(dose)'
+        model = ols(formula, datas).fit()
+        aov_table = anova_lm(model, typ=2)
+        eta_squared(aov_table)
+        omega_squared(aov_table)
+        
+        print aov_table
+        
+        #sub = data[[column1, column2, column3, column4]]
+        #sub_data = sub.copy()
+        
+        # data must be standardized
+        #warnings.filterwarnings('ignore') 
+        #seaborn.pairplot(sub_data);
+        #plt.show()
+        '''
         print ' '
-        print 'Currently working on it'
-        print ' '
-        exit()
-       
+
+            
 print ' '
 print 'Hasta la vista, human.'
 print ' '
